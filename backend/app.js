f4881dc9d8003f9ca2217e3a221671c4d5bde7dd
Hina Sato <yorushika@Hinas-Air.attlocal.net>
2023-12-28 20:04:47 -0600

    feat: user model

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import express from 'express'
 import { Sequelize } from 'sequelize';
+// import bcrypt from 'bcrypt'
 
 //--------------------------
 const seq = new Sequelize({
@@ -19,14 +20,62 @@ const User = seq.define('User', {
         autoIncrement: true,
     },
     username: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(30),
         allowNull: false,
     },
     password: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(30),
         allowNull: false,
-    }
+    },
+    email: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+    },
+    firstName: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+    },
+    dob: {
+        type: Sequelize.DATE,
+        allowNull: false,
+    },
+    age: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    bio: {
+        type: Sequelize.STRING(300),
+    },
+    city: {
+        type: Sequelize.STRING(30),
+    },
+    occupation: {
+        type: Sequelize.STRING(30),
+    },
+    gender: {
+        type: Sequelize.STRING(1),
+        allowNull: false,
+    },
+    preferences: {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+        allowNull: false,
+    },
+    minAge: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    maxAge: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    distance: {
+        type: Sequelize.INTEGER,
+    },
 });
+// User.beforeCreate(async (user) => {
+//     const hashp = await bcrypt.hash(user.password,10)
+//     user.password = hashp
+// })
 
 seq.sync()
 //--------------------------
@@ -36,7 +85,11 @@ const corsOptions = {
     origin: '*',  
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' 
   }
-  
+ 
+const port = 4000
+
+const app = express()
+
 app.use(cors(corsOptions))
 app.use(express.json())
 
@@ -49,6 +102,18 @@ app.post('/user', async (req, res) => { // Creates an account
     const user = await User.create({
         username: data.username,
         password: data.password,
+        email: data.email,
+        firstName: data.firstName,
+        gender: data.gender,
+        minAge: data.minAge,
+        maxAge: data.maxAge,
+        preferences: data.preferences,
+        dob: new Date(data.dob),
+        bio: data.bio,
+        city: data.city,
+        occupation: data.occupation,
+        distance: data.distance,
+        age: data.age,
     })
     res.json(user)
 })
