6cd36e8c10c65dcc7cf41bd2ef0c2f98b5c382f4
Zirolet <zirolet@gmail.com>
2024-01-17 11:03:44 -0600

    feat:started k8s, githubActions, and terraform

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -8,7 +8,7 @@ let seq;
 if (process.env.NODE_ENV == 'dev'){
     seq = new Sequelize({
         dialect: 'postgres',
-        database: 'DATING_DB',
+        database: process.env.DB_DB,
         host: 'postgres_dev',
         port: 5432,
         username: process.env.DB_USERNAME,
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV == 'dev'){
 else if (process.env.NODE_ENV == 'test'){
     seq = new Sequelize({
         dialect: 'postgres',
-        database: 'DATING_DB',
+        database: process.env.DB_DB,
         host: 'localhost',
         port: 5433,
         username: process.env.DB_USERNAME,
@@ -28,7 +28,7 @@ else if (process.env.NODE_ENV == 'test'){
 else{
     seq = new Sequelize({
         dialect: 'postgres',
-        database: 'DATING_DB',
+        database: process.env.DB_DB,
         host: process.env.DB_HOST,
         port: 5432,
         username: process.env.DB_USERNAME,
