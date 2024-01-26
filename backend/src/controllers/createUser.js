7ceefd5ee6886e0f1d4e9e26d29753a129d5db5c
Zirolet <zirolet@gmail.com>
2024-01-25 21:32:04 -0600

    idk

--- backend/src/controllers/createUser.js
+++ backend/src/controllers/createUser.js
@@ -12,39 +12,7 @@ export { createUser };
 async function createUser(req, res) {
   const data = req.body;
 
-<<<<<<< HEAD
-    const notValid = await validate(data) // Input Validation
-    if (notValid) {
-        return res.status(notValid.status).send(notValid.error)
-    }
-    else {
-        let date = new Date(data.dob) 
-        let currDate = new Date() 
-        let mili = currDate.getTime() - date.gitTime()
-        bcrypt.hash(data.password, 2, async function(err, hash) {
-            const user = await User.create({
-                username: data.username,
-                password: hash, // DO NOT SEND THIS!!!!
-                email: data.email,
-                firstName: data.firstName,
-                gender: data.gender,
-                minAge: data.minAge,
-                maxAge: data.maxAge,
-                preferences: data.preferences,
-                pronoun: data.pronoun,
-                dob: new Date(data.dob),
-                bio: data.bio,
-                city: data.city,
-                state: data.state,
-                occupation: data.occupation,
-                distance: data.distance,
-                age:,
-            })
-    
-            const sanitizedUser = { ...user.toJSON(), password: undefined}
-=======
   const notValid = await validate(data); // Input Validation
->>>>>>> refs/remotes/origin/dev
 
   // const invite = await Invite.findOne({where: {invite_key: data.ikey}})
 
@@ -83,39 +51,6 @@ async function createUser(req, res) {
   }
 }
 
-<<<<<<< HEAD
-// async function validate(data) {
-
-//     if (!("firstName" in data)) return { error: "First name not provided", status: 400}
-
-//     if (!("password" in data)) return { error: "Password not provided", status: 400}
-
-//     if (!("age" in data)) return { error: "Age not provided", status: 400}
-     
-//     if (!("minAge" in data)) return { error: "Min age not provided", status: 400}
-
-//     if (!("maxAge" in data)) return { error: "Max age not provided", status: 400}
-
-//     if (!("pronoun" in data)) return { error: "Pronoun not provided", status: 400}
-
-//     if (!("preferences" in data)) return { error: "Prefrences not provided", status: 400}
-
-//     if (!("state" in data)) return { error: "State not provided", status: 400}
-
-//     if (!("city" in data)) return { error: "City not provided", status: 400}
-
-async function validate(data) {
-    const requiredFields = ["firstName", "password", "age", "minAge", "maxAge", "pronoun", "preferences", "state", "city"];
-
-    for (const field of requiredFields) {
-        if (!(field in data)) {
-            return { error: `${field} not provided`, status: 400 };
-        }
-    }
-
-    return { success: "Validation passed", status: 200 };
-
-=======
 async function validate(data) {
   if (!('firstName' in data))
     return { error: 'First name not provided', status: 400 };
@@ -140,10 +75,6 @@ async function validate(data) {
   if (!('state' in data)) return { error: 'State not provided', status: 400 };
 
   if (!('city' in data)) return { error: 'City not provided', status: 400 };
->>>>>>> refs/remotes/origin/dev
-
-  if (!validGenders.includes(data.gender))
-    return { error: 'Incorrect gender value', status: 400 }; // Make sure the inputted gender is an option
 
   if (await User.findOne({ where: { username: data.username } }))
     return { error: 'Username is already taken', status: 400 }; // Username must be unique
